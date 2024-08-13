c3832948a3dc544b9418454202f76e5c8e3e5fd8
AlejandroIMP <alejandrosunuc@gmail.com>
2024-08-13 13:34:30 -0600

    ola

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const inputAmount = document.getElementsByid('mortgage-amount');
 const inputInterest = document.getElementsByid('mortgage-interest');
 const inputYears = document.getElementsByid('mortgage-years');
-
 const sectionResult = document.getElementsByClassName('empty--results');
+
+document.addEventListener("DOMContentLoaded");
 
 function deleteSectionItems(section) {
     while (section.firstChild) {
         section.removeChild(section.firstChild);
     }
 }
+
+
 handleClick = () => {
     deleteSectionItems(sectionResult);
 }
